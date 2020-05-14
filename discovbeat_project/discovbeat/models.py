2d35194850aa1e29d0a96e59e1fc5f20111094b8
Robert Nimmo <robertnimmo26@live.co.uk>
2020-05-14 02:31:12 +0100

    saving songs to database from playlist

--- discovbeat_project/discovbeat/models.py
+++ discovbeat_project/discovbeat/models.py
@@ -2,30 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
-class userPlaylist(models.Model):
-    sharedUser=models.ForeignKey(User,related_name="sharedUser", on_delete=models.CASCADE)
+class UserPlaylist(models.Model):
+    sharedUser=models.ForeignKey(User,related_name="sharedUser",null=True,blank=True, on_delete=models.CASCADE)
     owner=models.ForeignKey(User,related_name="owner", on_delete=models.CASCADE)
     slug = models.SlugField(unique=True)
     name= models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    playlistId= models.CharField(primary_key=True, max_length=100)
+    playlistId= models.CharField(primary_key=True, blank=True,max_length=100)
+    json=models.CharField(max_length=10000)
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.playlistId)
-        super(Quiz, self).save(*args, **kwargs)
+        super(UserPlaylist, self).save(*args, **kwargs)
 
-class song(models.Model):
-    playlist=models.ForeignKey(userPlaylist, on_delete=models.CASCADE)
-    slug = models.SlugField(unique=True)
+class Song(models.Model):
+    playlist=models.ForeignKey(UserPlaylist, on_delete=models.CASCADE)
     rating=models.BooleanField(default=False)
     comment=models.CharField(max_length=300)
     name=models.CharField(max_length=100)
     album=models.CharField(max_length=100)
-    artists=models.CharField(max_length=100)
-    songId= models.CharField(primary_key=True, max_length=100)
+    artist=models.CharField(max_length=100)
+    songId= models.CharField(max_length=100, null=True)
+    json=models.CharField(max_length=100000)
+    songAutoId = models.BigAutoField(primary_key=True)
+    slug = models.SlugField(unique=False)
+
+
 
     # In the future add restriction check
 
@@ -33,5 +38,5 @@ class song(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.playlistId)
-        super(Quiz, self).save(*args, **kwargs)
+        self.slug = slugify(self.songAutoId)
+        super(Song, self).save(*args, **kwargs)
--- discovbeat_project/discovbeat/views.py
+++ discovbeat_project/discovbeat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from social_django.models import *
+from discovbeat.models import UserPlaylist, Song
 
 import spotipy
 
@@ -8,12 +9,21 @@ import spotipy
 def index(request):
 	return render(request, 'discovbeat/index.html')
 
+def getUserSocialId(user):
+	return UserSocialAuth.objects.get(user=user)
+
+def getToken(user):
+	userSocialId=getUserSocialId(user)
+	extraData=userSocialId.extra_data
+	return extraData['access_token']
+
+
 def dashboard(request):
 
 	context_dict = {}
 	context_dict["userPlaylist"]=[]
 
-	userSocialId = UserSocialAuth.objects.get(user=request.user)
+	userSocialId = getUserSocialId(request.user)
 	username=userSocialId.uid
 	extraData=userSocialId.extra_data
 
@@ -48,10 +58,36 @@ def dashboard(request):
 	return render(request, 'discovbeat/dashboard.html', context=context_dict)
 
 def shareplaylist(request, playlist=None):
+	context_dict={}
+	songList=[]
 	for key,value in request.POST.items():
 		if key!="csrfmiddlewaretoken":
 			playlist=eval(value)
-			print(playlist)
-			print(key)
-			print(playlist['id'])
-	return render(request, 'discovbeat/shareplaylist.html')
+			user = User.objects.get(username = request.user)
+			newPlaylist=UserPlaylist(owner=user, name=playlist['name'],playlistId=playlist['id'],json=playlist)
+			newPlaylist.save()
+			token=getToken(request.user)
+			sp = spotipy.Spotify(auth=token)
+			results = sp.playlist(playlist['id'], fields="tracks,next")
+			tracks = results['tracks']
+			songList+=add_tracks(tracks,newPlaylist)
+			while tracks['next']:
+				tracks = sp.next(tracks)
+				songList+=add_tracks(tracks,newPlaylist)
+
+			for song in songList:
+				print(song.name)
+			context_dict['playlist']=newPlaylist
+			context_dict['songList']=songList
+
+
+	return render(request, 'discovbeat/shareplaylist.html', context=context_dict)
+
+def add_tracks(results,playlist):
+	songList=[]
+	for item in results['items']:
+		track = item['track']
+		song = Song(playlist=playlist,name=track['name'],artist=track['artists'][0]['name'],album=track['album']['name'],songId=track['id'],json=track)
+		song.save()
+		songList.append(song)
+	return songList
