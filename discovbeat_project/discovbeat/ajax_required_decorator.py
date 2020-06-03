a29d7c4b390ba937551ebee68d1e3d63f0a32125
Robert Nimmo <robertnimmo26@live.co.uk>
2020-06-04 00:35:52 +0100

    deleted unused file

--- discovbeat_project/discovbeat/ajax_required_decorator.py
+++
@@ -1,8 +0,0 @@
-def ajax_required(ret_unexcepted):
-    def _ajax_required(func):
-        def wrapper(request, *args, **kwargs):
-            if not request.is_ajax():
-                return ret_unexcepted
-            return func(request, *args, **kwargs)
-        return wrapper
-    return _ajax_required
