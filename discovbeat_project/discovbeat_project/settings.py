febabad6ba9f0da0dd0f26b42cd7d7074186cd70
Robert Nimmo <robertnimmo26@live.co.uk>
2020-06-05 16:28:15 +0100

    Changed DB for development

--- discovbeat_project/discovbeat_project/settings.py
+++ discovbeat_project/discovbeat_project/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'discovbeat_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'discovbeat$production',
-        'USER': 'discovbeat',
-        'PASSWORD': os.getenv("DBPASSWORD"),
-        'HOST': 'discovbeat.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'discovbeat$production',
+#         'USER': 'discovbeat',
+#         'PASSWORD': os.getenv("DBPASSWORD"),
+#         'HOST': 'discovbeat.mysql.pythonanywhere-services.com',
+#     }
+# }
 
 
 # Password validation
